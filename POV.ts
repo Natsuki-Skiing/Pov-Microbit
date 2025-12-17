d5ea2de3b3db308c3c3f292c3447e666646750de
Bill-Clinton <willjames1109@outlook.com>
2025-12-17 17:04:49 +0000

    Not working ?

--- POV.ts
+++ POV.ts
@@ -1,9 +1,1 @@
-namespace POV {
-    //% block
-    //% s.defl="hi"
-    export function showString(s: string) {
 
-    }
-    //% block
-    export function initlizePov() { }
-}
--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-POV.initlizePov()
+namespace POV {
+    //% block
+    //% s.defl="hi"
+    export function showString(s: string) {
+
+    }
+    //% block
+    export function initlizePov() { }
+}
