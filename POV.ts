0bfc759abace70a72a284a61b28411ea69de630e
Bill-Clinton <willjames1109@outlook.com>
2025-12-17 17:19:05 +0000

    Updated the file pointer

--- POV.ts
+++ POV.ts
@@ -1,1 +1,11 @@
+namespace POV {
+    //% block
+    //% s.defl="hi"
+    export function showString(s: string) {
 
+    }
+    //% block
+    export function initlizePov() { 
+        Basic.showString("POV Init");
+    }
+}
