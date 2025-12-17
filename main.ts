c76dd177f7b1b20de10ea4b3422a7b8c2309c49d
Natsuki-Skiing <44756358+Natsuki-Skiing@users.noreply.github.com>
2025-12-17 16:15:29 +0000

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
-})
+/**
+ * Use the VARIABLE.defl=VALUE notation to specify
+ * default argument values.
+ */
+
+//% color="#AA278D"
+namespace POV {
+    //% block
+    //% s.defl="hi"
+    export function showString(s: string) {
+
+    }
+}
