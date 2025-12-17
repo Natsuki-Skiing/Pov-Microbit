547eb1e7bfac63016fb6624507419b37018b19ef
Bill-Clinton <willjames1109@outlook.com>
2025-12-17 20:13:22 +0000

    Please work omg

--- POV.ts
+++ POV.ts
@@ -1,12 +1,12 @@
 namespace POV {
     //% block
     //% s.defl="hi"
-    //%shim=extension::showString
+    //% shim=extension::showString
     export function showString(s: string):void {
         
     }
     //% block
-    //%shim=extension::initPov
+    //% shim=extension::initPov
     export function initPov(): void { 
        
     }
