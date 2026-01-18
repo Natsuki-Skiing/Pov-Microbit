b7931b6841be8b91f368b7f2cc9330dc66fa4f81
Bill-Clinton <willjames1109@outlook.com>
2026-01-18 22:00:20 +0000

    Populated

---
+++ shims.d.ts
@@ -0,0 +1,14 @@
+// Auto-generated. Do not edit.
+
+declare namespace extension {
+
+    
+    //% shim=extension::init_Pov
+    function init_Pov(): void;
+
+   
+    //% shim=extension::show_String
+    function show_String(message: string): void;
+}
+
+// Auto-generated. Do not edit. Really.
